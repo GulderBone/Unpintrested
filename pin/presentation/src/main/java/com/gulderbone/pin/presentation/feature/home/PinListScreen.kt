8b31e9b208b779d6cbcaf7ccb9bf1ff37ab4e5ed
Bartkowiak <mateuszbartkowiak00@gmail.com>
2024-09-16 13:57:26 +0200

    ux

--- pin/presentation/src/main/java/com/gulderbone/pin/presentation/feature/home/PinListScreen.kt
+++ pin/presentation/src/main/java/com/gulderbone/pin/presentation/feature/home/PinListScreen.kt
@@ -1,16 +1,23 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.gulderbone.pin.presentation.feature.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -55,38 +62,47 @@ private fun PinListScreen(
         )
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-            .fillMaxSize()
-            .padding(horizontal = 16.dp)
-            .padding(vertical = 32.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceBetween
-    ) {
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            items(state.pins) { pin ->
-                PinCard(
-                    name = pin.name,
-                    value = pin.pin,
-                    isPinVisible = pin.isMasked,
-                    onTogglePinVisibility = { onAction(PinListAction.PinVisibilityChanged(pin.name, !pin.isMasked)) },
-                    onDelete = { onAction(PinListAction.DeletePinClicked(pin.name)) },
-                    modifier = Modifier.animateItem()
-                )
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    onAction(PinListAction.AddNewPinClicked)
+                }) {
+                Icon(Icons.Filled.Add, contentDescription = stringResource(R.string.add_new_pin))
             }
         }
-        Button(
-            onClick = {
-                onAction(PinListAction.AddNewPinClicked)
-            }) {
-            Text(stringResource(R.string.add_new_pin))
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .fillMaxSize()
+                .padding(horizontal = 16.dp)
+                .padding(top = 16.dp)
+                .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(state.pins) { pin ->
+                    PinCard(
+                        name = pin.name,
+                        value = pin.pin,
+                        isPinVisible = pin.isMasked,
+                        onTogglePinVisibility = { onAction(PinListAction.PinVisibilityChanged(pin.name, !pin.isMasked)) },
+                        onDelete = { onAction(PinListAction.DeletePinClicked(pin.name)) },
+                        modifier = Modifier.animateItem()
+                    )
+                }
+                item {
+                    Spacer(modifier = Modifier.height(64.dp))
+                }
+            }
         }
     }
 }
